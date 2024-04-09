f721f2ffa19a3192e0a39cc75eb562474b513c74
Khawaja Abdul Moiz <156238498+KhawajaAbdulMoiz@users.noreply.github.com>
2024-04-09 16:25:26 +0500

    CLI based Currency Convertor

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1,
+    EURO: 0.92,
+    INR: 83.28,
+    PKR: 278.12,
+    RIYAL: 3.75,
+    POUND: 0.79,
+    AUD: 1.52,
+    CAD: 1.36,
+    RUB: 92.58,
+};
+console.log(chalk.bgBlueBright.black("-------------------CURRENCY CONVERTOR-----------------\n"));
+let user_input = await inquirer.prompt([
+    {
+        name: "input",
+        message: chalk.bgGray("Select Currency : "),
+        type: "list",
+        choices: ["USD", "EURO", "INR", "PKR", "RIYAL", "POUND", "AUD", "CAD", "RUB"]
+    },
+    {
+        name: "input_to",
+        message: chalk.bgGreen("To Currency : "),
+        type: "list",
+        choices: ["USD", "EURO", "INR", "PKR", "RIYAL", "POUND", "AUD", "CAD", "RUB"]
+    },
+    {
+        name: 'amount',
+        message: chalk.red("Enter Amount : "),
+        type: 'number',
+    }
+]);
+let selectAmount = currency[user_input.input];
+let ToAmount = currency[user_input.input_to];
+let amount = user_input.amount;
+let baseAmount = amount / selectAmount;
+let convertedAmount = baseAmount * ToAmount;
+console.log(convertedAmount);
